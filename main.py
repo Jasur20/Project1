10c5e9373ff8eb0c1036defb85430cab81f513e4
Jasur20 <147597491+Jasur20@users.noreply.github.com>
2024-05-01 18:56:49 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,92 +1,65 @@
 import telebot
 from telebot import types
-
-bot=telebot.TeleBot("6470818555:AAFqBtXXGzUpYLH5YDsZYEvJrFTV-JBnYiY")
-
-@bot.message_handler(commands=['start','main','hello'])
-def main(message):
-    bot.send_message(message.chat.id,'Привет')
-
-#@bot.message_handler(commands=['help'])
-#def main(message):
-#    bot.send_message(message.chat.id,"<b>Help</b> <em>information</em>",parse_mode='html')
+import random
+import requests
 
 
+bot=telebot.TeleBot('6470818555:AAFqBtXXGzUpYLH5YDsZYEvJrFTV-JBnYiY')
 
-@bot.message_handler(commands=['help','stop'])
-def help(message):
-    if message.text=='/help':
-        bot.send_message(message.from_user.id,"/reg-регистрация\n"
-                                          "/help-помощь\n"
-                                          "/start-начать")
-    elif message.text=='/stop':
-        bot.send_message(message.from_user.id,"Проццес завершен")
-        return
+first = ["Сегодня — идеальный день для новых начинаний.","Оптимальный день для того, чтобы решиться на смелый поступок!","Будьте осторожны, сегодня звёзды могут повлиять на ваше финансовое состояние.","Лучшее время для того, чтобы начать новые отношения или разобраться со старыми.","Плодотворный день для того, чтобы разобраться с накопившимися делами."]
+second = ["Но помните, что даже в этом случае нужно не забывать про","Если поедете за город, заранее подумайте про","Те, кто сегодня нацелен выполнить множество дел, должны помнить про","Если у вас упадок сил, обратите внимание на","Помните, что мысли материальны, а значит вам в течение дня нужно постоянно думать про"]
+second_add = ["отношения с друзьями и близкими.","работу и деловые вопросы, которые могут так некстати помешать планам.","себя и своё здоровье, иначе к вечеру возможен полный раздрай.","бытовые вопросы — особенно те, которые вы не доделали вчера.","отдых, чтобы не превратить себя в загнанную лошадь в конце месяца."]
+third = ["Злые языки могут говорить вам обратное, но сегодня их слушать не нужно.","Знайте, что успех благоволит только настойчивым, поэтому посвятите этот день воспитанию духа.","Даже если вы не сможете уменьшить влияние ретроградного Меркурия, то хотя бы доведите дела до конца.","Не нужно бояться одиноких встреч — сегодня то самое время, когда они значат многое.","Если встретите незнакомца на пути — проявите участие, и тогда эта встреча посулит вам приятные хлопоты."]
 
+@bot.message_handler(commands=['help','start'])
+def get_help(message):
+    if message.text=="/start":
+        keyboard = types.InlineKeyboardMarkup()
 
-# @bot.message_handler(content_types=['text','audio','document'])
-# def get_text_messages(message):
-#     if message.text == "Привет":
-#         bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
-
-@bot.message_handler(commands=['help'])
-def help(message):
-    bot.send_message(message.from_user.id,"/reg-регистрация"
-                                          "/help-помощь"
-                                          "/start-начать")
-
-
-name=''
-surname=''
-age=0
+        key_info = types.InlineKeyboardButton(text='Информация о карте', callback_data='info')
+        keyboard.add(key_info)
+        key_balance = types.InlineKeyboardButton(text='Узнать баланс', callback_data='balance')
+        keyboard.add(key_balance)
+        key_last_tr = types.InlineKeyboardButton(text='Последняя транзакция', callback_data='last_tr')
+        keyboard.add(key_last_tr)
+        bot.send_message(message.from_user.id, text='Что Вы хотите узнать?', reply_markup=keyboard)
+    elif message.text=="/help":
+        bot.send_message(message.from_user.id,'hello!')
 
 @bot.message_handler(content_types=['text'])
-def start(message):
-    if message.text=="Привет":
-        bot.send_message(message.from_user.id,'Привет, чем я могу тебе помочь?')
-    elif message.text=='/reg':
-        bot.send_message(message.from_user.id,"Как тебя зовут?")
-        bot.register_next_step_handler(message,get_name)
+def get_text_message(message):
+    if message.text=="Привет" or message.text=="Hi":
+        bot.send_message(message.from_user.id,'Привет, чем я могу помочь?')
     else:
-        bot.send_message(message.from_user.id,'Напиши /reg`.')
-
-def get_name(message):
-    global name
-    name=message.text
-    bot.send_message(message.from_user.id,"Какая у теюя фамилия?")
-    bot.register_next_step_handler(message,get_surname)
-
-
-def get_surname(message):
-    global surname
-    surname=message.text
-    bot.send_message(message.from_user.id,"Сколько тебе лет?")
-    bot.register_next_step_handler(message,get_age)
-
-def get_age(message):
-    global age;
-    while age == 0:  # проверяем что возраст изменился
-        try:
-            age = int(message.text)  # проверяем, что возраст введен корректно
-        except Exception:
-            bot.send_message(message.from_user.id, 'Цифрами, пожалуйста');
-    keyboard = types.InlineKeyboardMarkup();  # наша клавиатура
-    key_yes = types.InlineKeyboardButton(text='Да', callback_data='yes');  # кнопка «Да»
-    keyboard.add(key_yes);  # добавляем кнопку в клавиатуру
-    key_no = types.InlineKeyboardButton(text='Нет', callback_data='no');
-    keyboard.add(key_no);
-    question = 'Тебе ' + str(age) + ' лет, тебя зовут ' + name + ' ' + surname + '?';
-    bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
+        bot.send_message(message.from_user.id,"Я тебя не понимаю, введите - /help")
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
-    if call.data == "yes": #call.data это callback_data, которую мы указали при объявлении кнопки
-        #код сохранения данных, или их обработки
-        bot.send_message(call.message.chat.id, 'Запомню : )');
-    elif call.data == "no":
-        bot.send_message(call.message.chat.id,"Тогда давай еще раз.")
-        return start
+  # Если нажали на одну из 12 кнопок — выводим гороскоп
+  if call.data == 'info':
+      BASE_URL = ('https://api.github.com')
+      query_params = {
+          "limit": 3
+        }
+      response = requests.get(f"{BASE_URL}", params=query_params)
+      print(response.text)
+      bot.send_message(call.message.chat.id, response.text)
+
+  if call.data=='balance':
+      BASE_URL = 'https://fakestoreapi.com'
+      query_params = {
+          "limit": 3
+      }
+      response = requests.get(f"{BASE_URL}/balance", params=query_params)
+      bot.send_message(call.message.chat.id,response.text)
+
+  if call.data=='last_tr':
+      BASE_URL = 'https://fakestoreapi.com'
+      query_params = {
+          "limit": 3
+      }
+      response = requests.get(f"{BASE_URL}/lastTranc", params=query_params)
 
 
-bot.polling(non_stop=True)
-#bot.infinity_polling()
+bot.infinity_polling()
+
