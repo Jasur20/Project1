ccd986f619d6756ac5407e8ef21844d45fc95d9b
Jasur20 <147597491+Jasur20@users.noreply.github.com>
2024-05-02 19:40:13 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,34 @@ from telebot import types
 import random
 import requests
 
-
 bot=telebot.TeleBot('6470818555:AAFqBtXXGzUpYLH5YDsZYEvJrFTV-JBnYiY')
 
-first = ["Сегодня — идеальный день для новых начинаний.","Оптимальный день для того, чтобы решиться на смелый поступок!","Будьте осторожны, сегодня звёзды могут повлиять на ваше финансовое состояние.","Лучшее время для того, чтобы начать новые отношения или разобраться со старыми.","Плодотворный день для того, чтобы разобраться с накопившимися делами."]
-second = ["Но помните, что даже в этом случае нужно не забывать про","Если поедете за город, заранее подумайте про","Те, кто сегодня нацелен выполнить множество дел, должны помнить про","Если у вас упадок сил, обратите внимание на","Помните, что мысли материальны, а значит вам в течение дня нужно постоянно думать про"]
-second_add = ["отношения с друзьями и близкими.","работу и деловые вопросы, которые могут так некстати помешать планам.","себя и своё здоровье, иначе к вечеру возможен полный раздрай.","бытовые вопросы — особенно те, которые вы не доделали вчера.","отдых, чтобы не превратить себя в загнанную лошадь в конце месяца."]
-third = ["Злые языки могут говорить вам обратное, но сегодня их слушать не нужно.","Знайте, что успех благоволит только настойчивым, поэтому посвятите этот день воспитанию духа.","Даже если вы не сможете уменьшить влияние ретроградного Меркурия, то хотя бы доведите дела до конца.","Не нужно бояться одиноких встреч — сегодня то самое время, когда они значат многое.","Если встретите незнакомца на пути — проявите участие, и тогда эта встреча посулит вам приятные хлопоты."]
+card_number=''
+password=''
+
+@bot.message_handler(content_types=['text'])
+def start(message):
+    global card_number
+    bot.message_handler(message.from_user.id,'Введите номер своей карты')
+    card_number=message.text
+    if len(card_number)!=16:
+        bot.send_message(message.from_user.id,'Неверный ввод номера карты.Попробуйте снова!!')
+    else:
+        bot.register_next_step_handler(message,get_password)
+
+def get_password(message):
+    global password
+    password=message.text
+    if len(password)<8:
+        bot.send_message(message.from_user.id,'Неверный пароль!!Повторите попытку.')
+    else:
+        bot.send_message(message.from_user.id,'Вы успешно вошли в свой личный кабинет')
+
+
 
 @bot.message_handler(commands=['help','start'])
 def get_help(message):
-    if message.text=="/start":
+    if message.text=="/help":
         keyboard = types.InlineKeyboardMarkup()
 
         key_info = types.InlineKeyboardButton(text='Информация о карте', callback_data='info')
@@ -22,8 +39,12 @@ def get_help(message):
         keyboard.add(key_balance)
         key_last_tr = types.InlineKeyboardButton(text='Последняя транзакция', callback_data='last_tr')
         keyboard.add(key_last_tr)
+        key_createac=types.InlineKeyboardButton(text='Создать личный кабинет',callback_data='create',url='https://brt.tj/ru')
+        keyboard.add(key_createac)
+        key_autorization=types.InlineKeyboardButton(text='Войти в личный кабинет',callback_data='in')
+        keyboard.add(key_autorization)
         bot.send_message(message.from_user.id, text='Что Вы хотите узнать?', reply_markup=keyboard)
-    elif message.text=="/help":
+    elif message.text=="/start":
         bot.send_message(message.from_user.id,'hello!')
 
 @bot.message_handler(content_types=['text'])
@@ -42,8 +63,14 @@ def callback_worker(call):
           "limit": 3
         }
       response = requests.get(f"{BASE_URL}", params=query_params)
-      print(response.text)
-      bot.send_message(call.message.chat.id, response.text)
+      if response.status_code==200:
+          bot.send_message(call.message.chat.id, response.text)
+      elif response.status_code==400:
+          bot.send_message(call.message.chat.id,'Данные введены некоректно.Исправьте.')
+      elif response.status_code==500:
+          bot.send_message(call.message.chat.id,'Сервер временно недоступен.')
+      else:
+          bot.send_message(call.message.chat.id,'Проблемы с сервисом.')
 
   if call.data=='balance':
       BASE_URL = 'https://fakestoreapi.com'
@@ -59,7 +86,12 @@ def callback_worker(call):
           "limit": 3
       }
       response = requests.get(f"{BASE_URL}/lastTranc", params=query_params)
+  if call.data=='in':
+      @bot.message_handler(content_types=['text'])
+      def st(message):
+          bot.send_message(message.from_user.id,'hi')
+      st(message="hello")
+
 
 
 bot.infinity_polling()
-
